937f834b6bf7e1c6e4a7180df0b63c0b51318596
Evan Johnson <ejohnson@uberdelta.com>
2018-06-19 14:17:27 -0400

    add animations

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-window.onscroll = function() {
-  scrollHandler();
-};
-
-var header = document.getElementById("rgx-header");
-if (window.pageYOffset < window.innerHeight / 4) {
-  header.classList.add("animated");
-  header.classList.add("fadeIn");
-}
-
-var elementsToAnimate = document.getElementsByClassName("animated");
+var elementsToAnimate = document.getElementsByClassName("shouldAnimate");
 
 function scrollHandler() {
-  for (var i = 0; i < elementsToAnimate.length; i++) {}
+  if (elementsToAnimate.length === 0) {
+    window.onscroll = null;
+    return;
+  }
+
+  for (var i = 0; i < elementsToAnimate.length; i++) {
+    var elem = elementsToAnimate[i];
+    if (window.scrollY + window.innerHeight * 0.8 > elem.offsetTop) {
+      elem.classList.add("animated");
+      if (
+        elem.classList.contains("rgx-iframe") ||
+        elem.classList.contains("rgx-screenshot")
+      ) {
+        elem.classList.add("fadeIn");
+      } else {
+        elem.classList.add("fadeInUp");
+      }
+
+      elem.classList.remove("shouldAnimate");
+      i--;
+    }
+  }
 }
+
+scrollHandler();
+
+window.onscroll = scrollHandler;
